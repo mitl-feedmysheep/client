d914f4bb07a08daf49768032a6e3922f69f5722f
martin-dsp <kcs19542001@hotmail.com>
2024-05-28 23:53:40 +0900

    first commit & splash screen applied

--- android/app/src/main/java/com/client/MainActivity.kt
+++ android/app/src/main/java/com/client/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.client
 
+import android.os.Bundle
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+import org.devio.rn.splashscreen.SplashScreen
 
 class MainActivity : ReactActivity() {
 
@@ -19,4 +23,9 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    SplashScreen.show(this);
+  }
 }
